882f7a89ce6f0c4e3a12bb5e24fa53c8a5ea7f88
Raphael Ferreira <raphaelbsferreira@gmail.com>
2018-01-25 19:58:08 -0200

    fixing production build

--- client/src/app/company/company.module.ts
+++ client/src/app/company/company.module.ts
@@ -7,13 +7,15 @@ import { CompanyFormContainerComponent } from './containers/company-form-contain
 import { SharedModule } from 'app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { CompanyService } from 'app/company/services/company.service';
+import { NewCompanyScreenComponent } from './screens/new-company-screen/new-company-screen.component';
 
 @NgModule({
   imports: [CommonModule, CompanyRoutingModule, FormsModule, SharedModule],
   declarations: [
     CompanyDetailScreenComponent,
     CompanyFormComponent,
-    CompanyFormContainerComponent
+    CompanyFormContainerComponent,
+    NewCompanyScreenComponent
   ],
   providers: [CompanyService]
 })
---
+++ client/src/app/company/screens/new-company-screen/new-company-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-company-screen',
+  templateUrl: './new-company-screen.component.html',
+  styles: []
+})
+export class NewCompanyScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
