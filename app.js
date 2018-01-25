b50ec6625d5c53fba3752469c2fe10b63a69b197
Raphael Ferreira <raphaelbsferreira@gmail.com>
2018-01-25 18:46:46 -0200

    fixed statics folder path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/api/v1', routes);
 
